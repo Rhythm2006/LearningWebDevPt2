e7fa5d9d28280ea76e2e970fb4d661869752cd96
Rhythm <rhythsanjeev2006@gmail.com>
2026-06-04 19:21:13 +0530

    Update 2

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -3,12 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-
+// https://github.com/acdlite/react-fiber-architecture
+//read this to know about reactfibre and reconciliation.
 function App() {
   let [count, setCount] = useState(0)
   const addValue = () =>{
     if(count<20){
       setCount(count+1)
+      setCount(count+1)
+      setCount(count+1)
+      setCount(count+1) 
+      setCount(count+1)
+      setCount(count+1)
+      setCount(count+1) 
+      // Because all four calls use the same old value of counter, React batches them together and ends up setting the state to 1 only once, not incrementing it four times.
+      // To increment multiple times, use:
+
+      // setCounter(c => c + 1);
     }
     
   }
