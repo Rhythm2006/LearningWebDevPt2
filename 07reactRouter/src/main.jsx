545a2ddf7d8fbf230dac834003eeb530b4ecd65a
Rhythm <rhythsanjeev2006@gmail.com>
2026-06-21 21:03:18 +0530

    Routing technique

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -9,16 +9,35 @@ import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
 import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+
+const routes = [{
+  path: '',
+  element: <Home/>
+},{
+  path: 'about',
+  element: <About/>
+},{
+  path: 'contact',
+  element: <Contact/>
+},{
+  path: 'user/:userid',
+  element: <User/>
+},{
+  path: 'github',
+  element: <Github/>,
+  loader: githubInfoLoader
+}]
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
-      <Route path='' element={<Home/>}/>
-      <Route path='about' element={<About/>}/>
-      <Route path='contact' element={<Contact/>}/>
-      <Route path='user/:userid' element={<User/>}/>
-      <Route path='github' 
-      loader={githubInfoLoader}
-      element={<Github/>}/>
+      {routes.map((routes,index)=>{
+        return(
+        <Route key={index}
+        path={routes.path}
+        element={routes.element}
+        loader={routes.loader}
+        />)
+      })}
     </Route>
   )
 )
